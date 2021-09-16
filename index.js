4f8794edbe47fc4651b6a3f93f25e670c76f3077
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-16 12:35:48 -0700

    commit tree 766e57bf210b431902c0a8043d8060c7f60d25c0

--- index.js
+++ index.js
@@ -10,13 +10,76 @@
  */
 
 let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  let payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+  return payable;
+};
+
+function createEmployeeRecord(employeeInfo) {
+  return {
+    firstName: employeeInfo[0],
+    familyName: employeeInfo[1],
+    title: employeeInfo[2],
+    payPerHour: employeeInfo[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(employeeRecords) {
+  return employeeRecords.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(timestamp) {
+  this.timeInEvents.push(createEvent.call(timestamp, "TimeIn"));
+  return this;
+}
+
+function createTimeOutEvent(timestamp) {
+  this.timeOutEvents.push(createEvent.call(timestamp, "TimeOut"));
+  return this;
+}
+
+function createEvent(eventType) {
+  const [date, hour] = this.split(" ");
+  return {
+    type: eventType,
+    date: date,
+    hour: parseInt(hour, 10),
+  };
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(
+    (timeInEvent) => timeInEvent.date === date
+  ).hour;
+  const timeOut = this.timeOutEvents.find(
+    (timeOutEvent) => timeOutEvent.date === date
+  ).hour;
+  return (timeOut - timeIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+function calculatePayroll(employeeRecords) {
+  const payroll = employeeRecords.reduce(
+    (memo, employee) => memo + allWagesFor.call(employee),
+    0
+  );
+  return payroll;
+}
+
+function findEmployeeByFirstName(employeeRecords, firstName) {
+  return employeeRecords.find((employee) => employee.firstName === firstName);
 }
